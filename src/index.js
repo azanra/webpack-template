467ce6563fa5cb61a0f2a9cd4a43ccd1b81564ab
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-14 08:53:12 +0700

    Configure webpack to handle img in html and js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./style.css";
+import img from "./asset/resource/img.png";
 
 console.log("Hello world!");
+const paraElement = document.querySelector("p");
+const imgElement = document.createElement("img");
+imgElement.src = img;
+paraElement.appendChild(imgElement);
